54e4e2b44da5fec69a0b444a49c1c72499fa25bd
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2019-03-23 07:42:36 +0800

    changed photo and style for contact photo

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,374 +1,370 @@
 <template>
-    <div>
-        <!--<div class="spacer"></div>-->
-        <!--style="height: 50px; border: 5px solid black;"-->
+  <div>
+    <!--<div class="spacer"></div>-->
+    <!--style="height: 50px; border: 5px solid black;"-->
 
-        <Head></Head>
-        <Logo></Logo>
-        <div class='tricorder'>
-            <!--<img src="https://res.cloudinary.com/dmglopmul/image/upload/v1544885305/projectPhotos/mariposaweb/tricorder.jpg">-->
-            <div class="container gradientMe">
-                <p>Contact Us</p>
-                <p>mariposaweb.net is here to answer any questions you may have and create an effective
-                    solution for your web development needs
-                </p>
-            </div>
-        </div>
-
-        <div class="outer-wrapper">
-            <div class="wrap-left">
-                <div class="container">
-                    <div class="wrapper">
-                        <div class="contain">
-                            <form action="https://formspree.io/dansimonson@mariposaweb.net" method="POST">
-                                <ul class="flex-outer">
-                                    <li>
-                                        <label for="name">Name</label>
-                                        <input type="text" name="name" id="name" placeholder="" />
-                                    </li>
-                                    <li>
-                                        <label for="email">Email</label>
-                                        <input name="email" type="email" id="email" placeholder="" />
-                                    </li>
-                                    <li>
-                                        <label for="message">Message</label>
-                                        <!-- style='border: 1px solid #333;' -->
-                                        <textarea rows="6" id="message" name="message" placeholder=""></textarea>
-                                    </li>
-                                    <li><button type="submit">Submit</button></li>
-                                </ul>
-                            </form>
-                        </div>
-                        <div class="grid">
-                            <div class="box">
-                                <img src="../assets/me.jpg" alt="daniel simonson" />
-                                <span>
-                                    I grew up in silicon valley with a dream to become a computer
-                                    programmer. I've spent a great deal of time thinking about the
-                                    technical aspects of web development so you don't have to.
-                                    Build your brand. Get new business. Email me to get
-                                    started.</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="wrap-right">
-                <div class="gmap_canvas">
-                    <iframe width="600" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=%20silang&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                        frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                    <a href="https://www.maps-erstellen.de"></a>
-                </div>
-            </div>
-
-        </div>
-        <Footer></Footer>
-
+    <Head></Head>
+    <Logo></Logo>
+    <div class="tricorder">
+      <!--<img src="https://res.cloudinary.com/dmglopmul/image/upload/v1553296357/projectPhotos/mariposaweb/tim-evans-88330-unsplash.jpg">-->
+      <div class="container gradientMe space">
+        <p>Contact Us</p>
+        <p>
+          mariposaweb.net is here to answer any questions you may have and create an effective
+          solution for your web development needs
+        </p>
+      </div>
     </div>
 
-
+    <div class="outer-wrapper">
+      <div class="wrap-left">
+        <div class="container">
+          <div class="wrapper">
+            <div class="contain">
+              <form action="https://formspree.io/dansimonson@mariposaweb.net" method="POST">
+                <ul class="flex-outer">
+                  <li>
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder>
+                  </li>
+                  <li>
+                    <label for="email">Email</label>
+                    <input name="email" type="email" id="email" placeholder>
+                  </li>
+                  <li>
+                    <label for="message">Message</label>
+                    <!-- style='border: 1px solid #333;' -->
+                    <textarea rows="6" id="message" name="message" placeholder></textarea>
+                  </li>
+                  <li>
+                    <button type="submit">Submit</button>
+                  </li>
+                </ul>
+              </form>
+            </div>
+            <div class="grid">
+              <div class="box">
+                <img src="../assets/me.jpg" alt="daniel simonson">
+                <span>
+                  I grew up in silicon valley with a dream to become a computer
+                  programmer. I've spent a great deal of time thinking about the
+                  technical aspects of web development so you don't have to.
+                  Build your brand. Get new business. Email me to get
+                  started.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="wrap-right">
+        <div class="gmap_canvas">
+          <iframe
+            width="600"
+            height="250"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=%20silang&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+          <a href="https://www.maps-erstellen.de"></a>
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 <script>
-    export default {
-        data: () => ({
-            valid: false,
-            name: "",
-            nameRules: [
-                v => !!v || "Name is required",
-                v => v.length <= 10 || "Name must be less than 10 characters"
-            ],
-            email: "",
-            emailRules: [
-                v => !!v || "E-mail is required",
-                v =>
-                /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-                "E-mail must be valid"
-            ],
-            showLogo: true,
-            name: "",
-            message: ""
-        }),
-        methods: {
-            handleScroll() {
-                if (window.scrollY > 30) {
-                    this.showLogo = false;
-                } else {
-                    this.showLogo = true;
-                }
-            }
-        },
-        created() {
-            window.addEventListener("scroll", this.handleScroll);
-        }
-    };
+export default {
+  data: () => ({
+    valid: false,
+    name: "",
+    nameRules: [
+      v => !!v || "Name is required",
+      v => v.length <= 10 || "Name must be less than 10 characters"
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v =>
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+        "E-mail must be valid"
+    ],
+    showLogo: true,
+    name: "",
+    message: ""
+  }),
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 30) {
+        this.showLogo = false;
+      } else {
+        this.showLogo = true;
+      }
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+};
 </script>
 <style scoped lang='scss'>
-    /*google Fonts*/
-    @import url("https://fonts.googleapis.com/css?family=Kalam");
-    @import url("https://fonts.googleapis.com/css?family=Great+Vibes|Montserrat");
-    @import "../../public/styles.scss";
+/*google Fonts*/
+@import url("https://fonts.googleapis.com/css?family=Kalam");
+@import url("https://fonts.googleapis.com/css?family=Great+Vibes|Montserrat");
+@import "../../public/styles.scss";
 
-    .tricorder {
-        height: 26vw;
-        width: 90vw;
-        /*border: 5px solid firebrick;*/
-        margin: 130px auto 0 auto;
-        background: url("https://res.cloudinary.com/dmglopmul/image/upload/v1544885305/projectPhotos/mariposaweb/tricorder.jpg");
-        background-position: cover;
-        background-repeat: no-repeat;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+.space {
+  padding: 2rem;
+}
+.tricorder {
+  height: 20vw;//26vw;
+  width: 90vw;
+  /*border: 5px solid firebrick;*/
+  margin: 130px auto 0 auto;
+  background: url("https://res.cloudinary.com/dmglopmul/image/upload/v1553297237/projectPhotos/mariposaweb/contact.jpg");
+  background-position: cover;
+  background-repeat: no-repeat;
+  background-color: brown;
+  background-blend-mode: darken;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  .container {
+    /*border: 5px solid orange;*/
+    color: $White;
+    font-size: 2rem;
+    margin-top: 10px;
+    display: flex;
 
-        .container {
-            /*border: 5px solid orange;*/
-            color: $White;
-            font-size: 2rem;
-            margin-top: 10px;
-            display: flex;
-
-            p {
-                margin: 0 auto;
-            }
-        }
+    p {
+      margin: 0 auto;
     }
+  }
+}
 
-    .outer-wrapper {
-        /*border: 5px solid black;*/
-        display: flex;
-        flex-direction: column;
+.outer-wrapper {
+  /*border: 5px solid black;*/
+  display: flex;
+  flex-direction: column;
+}
 
-    }
+.wrap-left {
+  width: 100%;
+  /*border: 5px solid green;*/
+}
 
-    .wrap-left {
-        width: 100%;
-        /*border: 5px solid green;*/
-    }
+.wrap-right {
+  width: 100%;
+  height: 100%;
+  margin-bottom: 5px;
+  /*border: 5px solid red;*/
+  display: block;
+}
 
-    .wrap-right {
-        width: 100%;
-        height: 100%;
-        margin-bottom: 5px;
-        /*border: 5px solid red;*/
-        display: block
-    }
+.container {
+  /*border: 5px solid blue;*/
+  width: 100%;
+  margin-top: 0;
+  margin-bottom: 0;
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+}
 
-    .container {
-        /*border: 5px solid blue;*/
-        width: 100%;
-        margin-top: 0;
-        margin-bottom: 0;
-        display: flex;
-        flex-wrap: wrap;
-        text-align: center;
+/*wrap my form and image/text sections*/
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  border: 1px solid $Grey;
+  max-width: 100%;
+  /*margin: 80px auto 0 auto;*/
+  margin: 80px 20px 10px 20px;
+  padding-left: 25px;
+  background: url("../assets/wrinkle.jpg");
+  background-repeat: no-repeat;
+  background-position: cover;
+  border-radius: 50px;
+}
 
-    }
+/*form*/
+.contain {
+  /*border: 5px solid black;*/
+  grid-column: 1/4;
+  max-width: 500px;
+  padding: 5px 0;
+  margin-bottom: 50px;
+}
 
-    /*wrap my form and image/text sections*/
-    .wrapper {
+textarea {
+  width: 350px;
+}
 
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        border: 1px solid $Grey;
-        max-width: 100%;
-        /*margin: 80px auto 0 auto;*/
-        margin: 80px 20px 10px 20px;
-        padding-left: 25px;
-        background: url("../assets/wrinkle.jpg");
-        background-repeat: no-repeat;
-        background-position: cover;
-        border-radius: 50px;
-    }
+.flex-outer li {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  flex: 0 1 auto;
+  flex-direction: column;
+  font-family: "Kalam", cursive;
+}
 
-    /*form*/
-    .contain {
-        /*border: 5px solid black;*/
-        grid-column: 1/4;
-        max-width: 500px;
-        padding: 5px 0;
-        margin-bottom: 50px;
-    }
+.flex-outer li button {
+  border: none;
+  background: #333;
+  color: #f2f2f2;
+  text-transform: uppercase;
+  letter-spacing: 0.09em;
+  border-radius: 2px;
+  font-family: "Kalam", cursive;
+}
 
-    textarea {
-        width: 350px;
-    }
-
-    .flex-outer li {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: flex-start;
-        flex: 0 1 auto;
-        flex-direction: column;
-        font-family: "Kalam", cursive;
-    }
-
-    .flex-outer li button {
-        border: none;
-        background: #333;
-        color: #f2f2f2;
-        text-transform: uppercase;
-        letter-spacing: 0.09em;
-        border-radius: 2px;
-        font-family: "Kalam", cursive;
-    }
-
-    .flex-outer li button:active,
-    .flex-outer li button:hover {
-        /*border: none;*/
-        color: #333;
-        background: white;
-        /*text-transform: uppercase;
+.flex-outer li button:active,
+.flex-outer li button:hover {
+  /*border: none;*/
+  color: #333;
+  background: white;
+  /*text-transform: uppercase;
           letter-spacing: .09em;
           border-radius: 2px;
           font-family: 'Kalam', cursive;*/
-    }
+}
 
-    /*image and text*/
-    .grid {
-        grid-column: 1/4;
-        margin-bottom: 50px;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-flex-direction: row;
-        flex-direction: row;
-        -webkit-align-items: center;
-        align-items: center;
-        flex-wrap: wrap;
-        /*border: 5px solid green;*/
-    }
+/*image and text*/
+.grid {
+  grid-column: 1/4;
+  margin-bottom: 50px;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: row;
+  flex-direction: row;
+  -webkit-align-items: center;
+  align-items: center;
+  flex-wrap: wrap;
+  /*border: 5px solid green;*/
+}
 
-    .box {
-        display: flex;
-        align-items: center;
-        /*border: 5px solid deeppink;*/
-        height: 100%;
-        width: 100%;
-        margin: 5px;
-        padding: 0 30px 0 2px;
-        /*padding: 10px 2px 2px 10px;*/
-        text-align: justify;
-    }
+.box {
+  display: flex;
+  align-items: center;
+  /*border: 5px solid deeppink;*/
+  height: 100%;
+  width: 100%;
+  margin: 5px;
+  padding: 0 30px 0 2px;
+  /*padding: 10px 2px 2px 10px;*/
+  text-align: justify;
+}
 
-    .box img {
-        flex-grow: 0;
-        flex-shrink: 0;
-        border-radius: 50%;
-        max-width: 100px;
-        max-height: 100px;
-    }
+.box img {
+  flex-grow: 0;
+  flex-shrink: 0;
+  border-radius: 50%;
+  max-width: 100px;
+  max-height: 100px;
+}
 
-    span {
-        padding-left: 5px;
-        font-family: "Kalam", cursive;
-        font-size: 1.2em;
-    }
+span {
+  padding-left: 5px;
+  font-family: "Kalam", cursive;
+  font-size: 1.2em;
+}
 
-    #divfix {
-        font-family: "Great Vibes", cursive;
-        top: 6px;
-        left: 75px;
-        position: fixed;
-        z-index: 3000;
-        color: #ffe082;
-        padding: 5px;
-        font-size: 2em;
-    }
+#divfix {
+  font-family: "Great Vibes", cursive;
+  top: 6px;
+  left: 75px;
+  position: fixed;
+  z-index: 3000;
+  color: #ffe082;
+  padding: 5px;
+  font-size: 2em;
+}
 
-    #name,
-    #email {
-        border: 1px solid #333;
-        width: 80%;
-    }
+#name,
+#email {
+  border: 1px solid #333;
+  width: 80%;
+}
 
-    #message {
-        border: 1px solid #333;
-        width: 95%;
-    }
+#message {
+  border: 1px solid #333;
+  width: 95%;
+}
 
-    .gmap_canvas {
-        position: relative;
-        /*padding-bottom: calc(height / width);*/
-        padding-bottom: 75%;
-        height: 0;
-        overflow: hidden;
-        padding-left: 5px;
-        padding-right: 5px;
-        border-radius: 2%
-    }
+.gmap_canvas {
+  position: relative;
+  /*padding-bottom: calc(height / width);*/
+  padding-bottom: 75%;
+  height: 0;
+  overflow: hidden;
+  padding-left: 5px;
+  padding-right: 5px;
+  border-radius: 2%;
+}
 
-    .wrap-right {
-        margin-top: 90px;
-        margin-right: 10px;
-        margin-bottom: 10px;
-        /*border: 5px solid black;*/
+.wrap-right {
+  margin-top: 90px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  /*border: 5px solid black;*/
+}
 
-    }
+.gmap_canvas iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  height: 100% !important;
+}
 
-    .gmap_canvas iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100% !important;
-        height: 100% !important;
-    }
-
-    /*media Queries*/
-    @media only screen and (max-width: 360px) {
-        .container {
-
-            p {
-
-                /*font-size: 1rem;
+/*media Queries*/
+@media only screen and (max-width: 360px) {
+  .container {
+    p {
+      /*font-size: 1rem;
                 line-height: 1.5;*/
-            }
-
-        }
-
     }
+  }
+}
 
-    @media only screen and (max-width: 500px) {
-        .container {
-            /*border: 5px solid red;*/
-            margin-top: 0;
-            margin-bottom: 0;
+@media only screen and (max-width: 500px) {
+  .container {
+    /*border: 5px solid red;*/
+    margin-top: 0;
+    margin-bottom: 0;
 
-            p {
-                line-height: 1;
-                font-size: 1rem;
-
-            }
-
-        }
-
-        .box {
-            flex-direction: column;
-        }
-
+    p {
+      line-height: 1;
+      font-size: 1rem;
     }
+  }
 
-    @media only screen and (max-width: 768px) {
-        .container {
-            font-size: 1rem;
-        }
-    }
+  .box {
+    flex-direction: column;
+  }
+}
 
-    @media only screen and (min-width: 1800px) {
-        .tricorder {
-            background: url("https://res.cloudinary.com/dmglopmul/image/upload/v1544922900/projectPhotos/mariposaweb/tricorderbig.jpg");
-        }
+@media only screen and (max-width: 768px) {
+  .container {
+    font-size: 1rem;
+  }
+}
 
-        .wrapper {
-            background: url("https://res.cloudinary.com/dmglopmul/image/upload/v1544923408/projectPhotos/mariposaweb/wrinklebig.jpg");
+@media only screen and (min-width: 1800px) {
+  .tricorder {
+    background: url("https://res.cloudinary.com/dmglopmul/image/upload/v1544922900/projectPhotos/mariposaweb/tricorderbig.jpg");
+  }
 
-        }
-    }
+  .wrapper {
+    background: url("https://res.cloudinary.com/dmglopmul/image/upload/v1544923408/projectPhotos/mariposaweb/wrinklebig.jpg");
+  }
+}
 
-
-
-
-
-    /*@media only screen and (max-width: 768px) {
+/*@media only screen and (max-width: 768px) {
 
             .outer-wrapper {
                 flex-direction: column;
